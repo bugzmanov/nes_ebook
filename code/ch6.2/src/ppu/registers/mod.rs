f4fd933e1ca3822c4dfcb32654cc10495d7745e3
bugzmanov <bugzmanov@gmail.com>
2020-07-27 17:19:34 -0400

    ch6.2 - interrupts - code

---
+++ code/ch6.2/src/ppu/registers/mod.rs
@@ -0,0 +1,5 @@
+pub mod control;
+pub mod mask;
+pub mod status;
+pub mod scroll;
+pub mod addr;
---
+++ code/ch6.2/src/ppu/registers/status.rs
@@ -0,0 +1,63 @@
+bitflags! {
+
+    // 7  bit  0
+    // ---- ----
+    // VSO. ....
+    // |||| ||||
+    // |||+-++++- Least significant bits previously written into a PPU register
+    // |||        (due to register not being updated for this address)
+    // ||+------- Sprite overflow. The intent was for this flag to be set
+    // ||         whenever more than eight sprites appear on a scanline, but a
+    // ||         hardware bug causes the actual behavior to be more complicated
+    // ||         and generate false positives as well as false negatives; see
+    // ||         PPU sprite evaluation. This flag is set during sprite
+    // ||         evaluation and cleared at dot 1 (the second dot) of the
+    // ||         pre-render line.
+    // |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+    // |          a nonzero background pixel; cleared at dot 1 of the pre-render
+    // |          line.  Used for raster timing.
+    // +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+    //            Set at dot 1 of line 241 (the line *after* the post-render
+    //            line); cleared after reading $2002 and at dot 1 of the
+    //            pre-render line.
+    pub struct StatusRegister: u8 {
+        const NOTUSED          = 0b00000001;
+        const NOTUSED2         = 0b00000010;
+        const NOTUSED3         = 0b00000100;
+        const NOTUSED4         = 0b00001000;
+        const NOTUSED5         = 0b00010000;
+        const SPRITE_OVERFLOW  = 0b00100000;
+        const SPRITE_ZERO_HIT  = 0b01000000;
+        const VBLANK_STARTED   = 0b10000000;
+    }
+}
+
+impl StatusRegister {
+    pub fn new() -> Self {
+        StatusRegister::from_bits_truncate(0b00000000)
+    }
+
+    pub fn set_vblank_status(&mut self, status: bool) {
+        self.set(StatusRegister::VBLANK_STARTED, status);
+    }
+
+    pub fn set_sprite_zero_hit(&mut self, status: bool) {
+        self.set(StatusRegister::SPRITE_ZERO_HIT, status);
+    }
+
+    pub fn set_sprite_overflow(&mut self, status: bool) {
+        self.set(StatusRegister::SPRITE_OVERFLOW, status);
+    }
+
+    pub fn reset_vblank_status(&mut self) {
+        self.remove(StatusRegister::VBLANK_STARTED);
+    }
+
+    pub fn is_in_vblank(&self) -> bool {
+        self.contains(StatusRegister::VBLANK_STARTED)
+    }
+
+    pub fn snapshot(&self) -> u8 {
+        self.bits
+    }
+}
